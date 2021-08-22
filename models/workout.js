3c0d86b6b2f7b6f3df9aaf573a51d1f8a7acb783
Zdeal <jedimastershake@gmail.com>
2021-08-22 00:48:05 -0400

    model built

--- models/workout.js
+++ models/workout.js
@@ -6,5 +6,39 @@ const trackerSchema = new Schema({
     date: {
         type: Date,
         default: Date.now
-    }
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: true,
+            },
+            name: {
+                type: String,
+                trim: true, 
+                required: true,
+            },
+            duration: {
+                type: Number,
+                required: true,
+            },
+            weight: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            },
+        },
+    ]
 })
+
+const trackerSchema = mongoose.model("trackerSchema", workout);
+
+module.exports = trackerSchema
