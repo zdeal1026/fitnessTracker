5993a560833c5ca1fe52734ed9bbbb2589f553d5
Zdeal <jedimastershake@gmail.com>
2021-08-23 23:15:23 -0400

    debugging

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,15 +3,15 @@ const router = require("express").Router();
 
 //html routes
 router.get("/", (req,res) => {
-    res.sendFile(path.join(_dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 })
 
 router.get("/exercise", (req,res) => {
-    res.sendFile(path.join(_dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", (req,res) => {
-    res.sendFile(path.join(_dirname, "../public/stats/html"));
+    res.sendFile(path.join(__dirname, "../public/stats/html"));
 })
 
 //exporting
