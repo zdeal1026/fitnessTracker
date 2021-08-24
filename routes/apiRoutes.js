7466a020a8ba77a44dfaeeb6018296c030fbfb29
Zdeal <jedimastershake@gmail.com>
2021-08-23 22:14:42 -0400

    working apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,31 @@
 const db = require("../models");
 const router = require("express").Router();
 
+//getting workouts
+router.get("/api/workouts", (req, res) => {
+    db.workouts.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  //create new workout
+
+  router.post("/api/workouts", (req,res) => {
+      db.workouts.create(req.body)
+      .then((workout) => {
+          res.json(workout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+
+
+
+
+  module.exports = router
