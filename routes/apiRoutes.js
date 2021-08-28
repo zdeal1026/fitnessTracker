b801cb9740c979b17e893e09683af3c894d412a7
Zdeal <jedimastershake@gmail.com>
2021-08-28 09:57:50 -0400

    debugging heroku errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,6 +15,27 @@ router.get("/api/workouts", (req, res) => {
     res.json(err);
   })});
 
+  
+   //gettting week data with aggregate
+   router.get("/api/workouts/range", (req, res) => {
+    db.Workout.aggregate([
+      { $set: {
+        totalDuration: { $sum : "$exercises.duration"},
+      },
+    },
+  ]  
+    ).sort({ "day": -1 })
+    .limit(7)
+    
+    .then((workouts) => {
+      workouts.reverse();
+      res.json(workouts);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json(err);
+    });
+  });
   //create new workout
 
   router.post("/api/workouts", (req,res) => {
@@ -38,27 +59,6 @@ router.put("/api/workouts/:id", (req, res) => {
       res.status(400).json(err);
     });
  });
-
- //gettting week data with aggregate
- router.get("/api/workouts/range", (req, res) => {
-  db.Workout.aggregate([
-    { $set: {
-      totalDuration: { $sum : "$exercises.duration"},
-    },
-  },
-]  
-  ).sort({ "day": -1 })
-  .limit(7)
-  
-  .then((workouts) => {
-    workouts.reverse();
-    res.json(workouts);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.json(err);
-  });
-});
 
 
 
